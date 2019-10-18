99599261d836d633ac3932977ca93bad8b3bf304
Artur Bani <arturbani@gmail.com>
2019-10-18 17:18:08 -0300

    Updates

---
+++ precache-manifest.35fcf4f72e4efdec1ead5aa9942b04fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ebcca589dec2e744b7e0051bffad31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe92425a0b05a8a95c73",
+    "url": "./static/css/main.48e0b4bd.chunk.css"
+  },
+  {
+    "revision": "562d7267bc29f1600d2a",
+    "url": "./static/js/2.f49ec7ae.chunk.js"
+  },
+  {
+    "revision": "fe92425a0b05a8a95c73",
+    "url": "./static/js/main.fef3b308.chunk.js"
+  },
+  {
+    "revision": "fd4a65e8ad5d7bf2a823",
+    "url": "./static/js/runtime-main.e100eb0c.js"
+  },
+  {
+    "revision": "ed47b38b91c67d881563cc209e604efa",
+    "url": "./static/media/nh-logo.ed47b38b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb37dfdadd59118fe6f873be815f22f4.js"
+  "./precache-manifest.35fcf4f72e4efdec1ead5aa9942b04fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
