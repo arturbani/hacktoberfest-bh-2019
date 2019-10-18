9e34c51bb09b3e0128a06b54338b3846c9120874
Artur Bani <arturbani@gmail.com>
2019-10-18 16:57:03 -0300

    Added mock card to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 
+import HacktoberfestLogo from 'assets/icons/hacktoberfest'
+import NataHouseIcon from 'assets/icons/natahouse'
+import { Card } from 'components'
+
 import styles from './Home.module.scss'
+import imagemNH from '../../assets/images/nh-logo.png'
 
 const Home = () => {
   return (
-    <div>
-      <div>Hacktoberfest BH 2019</div>
-      <main>Content</main>
+    <div className={styles.container}>
+      <header className={styles.header}>Hacktoberfest BH 2019</header>
+      <main>
+        <div className={styles.content}>
+          <Card
+            name="nata.house"
+            role="Fábrica de Software"
+            image={imagemNH}
+            description="Estamos muito felizes em reunir essa moçada pra contribuir com a comunidade open-source \o/."
+          />
+        </div>
+      </main>
+      <div className={styles.footer}>
+        <div>
+          <HacktoberfestLogo /> <NataHouseIcon />
+        </div>
+      </div>
     </div>
   )
 }
