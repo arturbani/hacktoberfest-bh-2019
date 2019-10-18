4dd13a1b8309edec875692d3e9e6bb9969f99aba
Artur Bani <arturbani@gmail.com>
2019-10-18 17:14:02 -0300

    Updates

---
+++ precache-manifest.eb37dfdadd59118fe6f873be815f22f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666d8aab38b66eb7ff9d037be5f9ddd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1aded44b8605acea401",
+    "url": "/static/css/main.48e0b4bd.chunk.css"
+  },
+  {
+    "revision": "562d7267bc29f1600d2a",
+    "url": "/static/js/2.f49ec7ae.chunk.js"
+  },
+  {
+    "revision": "e1aded44b8605acea401",
+    "url": "/static/js/main.a5acaca8.chunk.js"
+  },
+  {
+    "revision": "367f5ab13bc02703db68",
+    "url": "/static/js/runtime-main.91e9fe81.js"
+  },
+  {
+    "revision": "ed47b38b91c67d881563cc209e604efa",
+    "url": "/static/media/nh-logo.ed47b38b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hacktoberfest-bh-2019/precache-manifest.5fbe80432dd54dc4dd7e426f0033d87b.js"
+  "/precache-manifest.eb37dfdadd59118fe6f873be815f22f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hacktoberfest-bh-2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
