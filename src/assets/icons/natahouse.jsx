d932368dd49b34b96a6fdf3e35a2925393e11336
Artur Bani <arturbani@gmail.com>
2019-10-18 16:56:26 -0300

    Added NH logo and image

---
+++ src/assets/icons/natahouse.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NataHouseIcon = () => (
+  <svg
+    class=""
+    aria-label="logo nata.house"
+    aria-hidden="false"
+    xmlns="http://www.w3.org/2000/svg"
+    height="136"
+    width="136"
+    viewBox="-2 0 42 42"
+  >
+    <g fill-rule="evenodd">
+      <path
+        fill="#15E1D5"
+        d="M36.208 19.467v21.508h-4.503l-10.088-6.569V18.852l1.531-1.377 2.973 2.754v11.725l5.584 3.636V21.573l-8.557-7.334-8.556 7.334v19.402h-4.504L0 34.406V10.988L13.06 0l8.14 6.847-3.471 2.975-4.669-3.928-8.556 7.2v18.86l5.584 3.636V19.493l13.06-10.989z"
+      />
+    </g>
+  </svg>
+)
+
+export default NataHouseIcon
